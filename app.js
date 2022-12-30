abca3109fabb954ac17917c67348b9f04f9a528a
eocampo2021 <114036603+eocampo2021@users.noreply.github.com>
2022-12-30 02:59:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,35 @@ const port = 8080;
 
 app.get('/products', async (req, res) =>{
   const productos = await productManager.getAllProduct();
-    // console.log('productos');
-  const { limit } = req.query;
+     //console.log(productos);
+  const limit = req.query.limit;
 
-  if (limit) return res.send(productos.slice(0, limit));
-  else return res.send(productos);
+  if (limit != null ) {
+    res.send(productos.slice(0, limit))
+  }else{
+
+    res.send(productos)
+  }
 
   });
-/*
-  app.get('/products/:pid', async (req, res) =>{
-    const productos = await productManager.getAllProduct();
+
+
+  app.get('/products/:pid',  (req, res) =>{
+    const pid = parseInt(req.params.pid);
+    console.log(pid)
+    //console.log(isNaN(pid))
+
+    if (pid != null ) {
+
+      const productos =  productManager.getProductById(pid);
+      res.send(productos)
+    }//else{
+  
+      //res.send(productos)
+   // }
     
   
     });
-*/
 
 
 app.listen (port, () => {
